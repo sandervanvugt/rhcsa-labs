c71d5206c1b1d4213e1b68f8a0df229cd1b84ea2
sandervanvugt <mail@sandervanvugt.nl>
2024-02-29 08:27:33 +0100

    message

--- exam-task5.sh
+++ exam-task5.sh
@@ -1,64 +1,98 @@
-if [ -b /dev/sdb ]
+if [ -b /dev/?db ]
 then
-	echo -e "\033[32m[OK]\033[0m\t\t block device \033[1m/dev/sdb\033[0m was found"
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t block device \033[1m/dev/sdb\033[0m was not found"
-fi
-
-
-if [ -b /dev/sdb1 ] && [ -b /dev/sdb2 ]
-then
-	echo -e "\033[32m[OK]\033[0m\t\t partitions \033[1m/dev/sdb1\033[0m and \033[1m/dev/sdb2\033[0m were found"
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t partitions \033[1m/dev/sdb1\033[0m and \033[1m/dev/sdb2\033[0m were not found"
-fi
-TOTAL=$(( TOTAL + 10 ))
-
-
-if fdisk -l /dev/sdb | grep gpt &>/dev/null
-then
-	echo -e "\033[32m[OK]\033[0m\t\t disk uses a GPT"
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t disk doesn't use a GPT"
-	SCORE=$(( SCORE - 10 ))
-fi
-TOTAL=$(( TOTAL + 10 ))
-
-
-if fdisk -l /dev/sdb | awk '/sdb1/ && /2G/' &>/dev/null
-then
-	echo -e "\033[32m[OK]\033[0m\t\t the first partition has a size of 2G"
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t incorrect size on first partition"
-fi
-TOTAL=$(( TOTAL + 10 ))
-
-
-if [[ $(fdisk -l /dev/sdb | awk '/sdb2/ && /5G/ && /LVM/' &>/dev/null) ]]
-then
-	echo -e "\033[32m[OK]\033[0m\t\t the second partition has a size of 5G and is marked as LVM partition"
-	SCORE=$(( SCORE + 10 ))
-else
-	if [[ $(fdisk -l /dev/sdb | awk '/sdb2/ && !/lvm/') ]]
+	echo -e "\033[32m[OK]\033[0m\t\t block device \033[1m/dev/?db\033[0m was found"
+	if [ -b /dev/?db1 ] && [ -b /dev/?db2 ]
 	then
-		echo -e "\033[31m[FAIL]\033[0m\t\t type of the second partition is incorrect"
+		echo -e "\033[32m[OK]\033[0m\t\t partitions \033[1m/dev/?db1\033[0m and \033[1m/dev/?db2\033[0m were found"
+		SCORE=$(( SCORE + 10 ))
+	else
+		echo -e "\033[31m[FAIL]\033[0m\t\t partitions \033[1m/dev/?db1\033[0m and \033[1m/dev/?db2\033[0m were not found"
 	fi
-	if [[ $(fdisk -l /dev/sdb | awk '/sdb2/ && !/5G/') ]]
-        then
-                echo -e "\033[31m[FAIL]\033[0m\t\t size of the second partition is not correct"
-        fi
+	TOTAL=$(( TOTAL + 10 ))
+
+
+	if fdisk -l /dev/?db | grep gpt &>/dev/null
+	then
+		echo -e "\033[32m[OK]\033[0m\t\t disk uses a GPT"
+		SCORE=$(( SCORE + 10 ))
+	else
+		echo -e "\033[31m[FAIL]\033[0m\t\t disk doesn't use a GPT"
+		SCORE=$(( SCORE - 10 ))
+	fi
+	TOTAL=$(( TOTAL + 10 ))
+
+	if fdisk -l /dev/?db | awk '/?db1/ && /2G/' &>/dev/null
+	then
+		echo -e "\033[32m[OK]\033[0m\t\t the first partition has a size of 2G"
+		SCORE=$(( SCORE + 10 ))
+	else
+		echo -e "\033[31m[FAIL]\033[0m\t\t incorrect size on first partition"
+	fi
+	TOTAL=$(( TOTAL + 10 ))
+
+	if [[ $(fdisk -l /dev/?db | awk '/?db2/ && /5G/ && /LVM/' &>/dev/null) ]]
+	then
+		echo -e "\033[32m[OK]\033[0m\t\t the second partition has a size of 5G and is marked as LVM partition"
+		SCORE=$(( SCORE + 10 ))
+	else
+		if [[ $(fdisk -l /dev/?db | awk '/?db2/ && !/lvm/') ]]
+		then
+			echo -e "\033[31m[FAIL]\033[0m\t\t type of the second partition is incorrect"
+		fi
+		if [[ $(fdisk -l /dev/?db | awk '/?db2/ && !/5G/') ]]
+       		then
+               		echo -e "\033[31m[FAIL]\033[0m\t\t size of the second partition is not correct"
+        	fi
+	fi
+	TOTAL=$(( TOTAL + 10 ))
 fi
-TOTAL=$(( TOTAL + 10 ))
 
-
-if true
+if [ -b /dev/nvme0n2 ]
 then
-	echo -e "\033[32m[OK]\033[0m\t\t "
-	SCORE=$(( SCORE + 10 ))
-else
-	echo -e "\033[31m[FAIL]\033[0m\t\t "
+        echo -e "\033[32m[OK]\033[0m\t\t block device \033[1m/dev/nvme0n2\033[0m was found"
+        if [ -b /dev/nvme0n2p1 ] && [ -b /dev/nvme0n2p2 ]
+        then
+                echo -e "\033[32m[OK]\033[0m\t\t partitions \033[1m/dev/nvme0n2p1\033[0m and \033[1m/dev/nvme0n2p2\033[0m were found"
+                SCORE=$(( SCORE + 10 ))
+        else
+                echo -e "\033[31m[FAIL]\033[0m\t\t partitions \033[1m/dev/nvme0n2p1\033[0m and \033[1m/dev/nvme0n2p2\033[0m were not found"
+        fi
+        TOTAL=$(( TOTAL + 10 ))
+
+
+        if fdisk -l /dev/nvme0n2 | grep gpt &>/dev/null
+        then
+                echo -e "\033[32m[OK]\033[0m\t\t disk uses a GPT"
+                SCORE=$(( SCORE + 10 ))
+        else
+                echo -e "\033[31m[FAIL]\033[0m\t\t disk doesn't use a GPT"
+                SCORE=$(( SCORE - 10 ))
+        fi
+        TOTAL=$(( TOTAL + 10 ))
+
+        if fdisk -l /dev/nvme0n2 | awk '/nvme0n2p1/ && /2G/' &>/dev/null
+        then
+                echo -e "\033[32m[OK]\033[0m\t\t the first partition has a size of 2G"
+                SCORE=$(( SCORE + 10 ))
+        else
+                echo -e "\033[31m[FAIL]\033[0m\t\t incorrect size on first partition"
+        fi
+        TOTAL=$(( TOTAL + 10 ))
+
+        if [[ $(fdisk -l /dev/nvme0n2 | awk '/nvme0n2p2/ && /5G/ && /LVM/' &>/dev/null) ]]
+        then
+                echo -e "\033[32m[OK]\033[0m\t\t the second partition has a size of 5G and is marked as LVM partition"
+                SCORE=$(( SCORE + 10 ))
+        else
+                if [[ $(fdisk -l /dev/nvme0n2 | awk '/nvme0n2p2/ && !/lvm/') ]]
+                then
+                        echo -e "\033[31m[FAIL]\033[0m\t\t type of the second partition is incorrect"
+                fi
+                if [[ $(fdisk -l /dev/nvme0n2 | awk '/nvme0n2p2/ && !/5G/') ]]
+                then
+                        echo -e "\033[31m[FAIL]\033[0m\t\t size of the second partition is not correct"
+                fi
+        fi
+        TOTAL=$(( TOTAL + 10 ))
 fi
-TOTAL=$(( TOTAL + 10 ))
+
