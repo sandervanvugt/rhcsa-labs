aab8e9b58d9f2089853cc76d77292e7d425eb5c0
sandervanvugt <mail@sandervanvugt.nl>
2024-02-29 11:11:21 +0100

    message

--- exam-task2.sh
+++ exam-task2.sh
@@ -9,11 +9,16 @@ TOTAL=$(( TOTAL + 10 ))
 
 find / -type f -size +200M -exec ls -l {} \; >/root/newfiles.txt 2>/dev/null
 
-if diff /root/bigfiles.txt /root/newfiles.txt &>/dev/null
+# making files with file names only to be able to use diff
+awk '{ print $NF }' /root/bigfiles.txt > /root/bigfilenames.txt
+awk '{ print $NF }' /root/newfiles.txt > /root/newfilenames.txt
+
+# checking if command output is correct
+if diff /root/bigfilenames.txt /root/newfilenames.txt &>/dev/null
 then
-	echo -e "\033[32m[OK]\033[0m\t\t /root/bigfiles has a list of all files bigger than 200M"
+	echo -e "\033[32m[OK]\033[0m\t\t /root/bigfiles.txt has a list of all files bigger than 200M"
 	SCORE=$(( SCORE + 10 ))
 else
-	echo -e "\033[31m[FAIL]\033[0m\t\t /root/bigfiles doesn't seem to have a long listing of all files bigger than 200M"
+	echo -e "\033[31m[FAIL]\033[0m\t\t /root/bigfiles.txt doesn't seem to have a long listing of all files bigger than 200M that matches your current system state"
 fi
 TOTAL=$(( TOTAL + 10 ))
