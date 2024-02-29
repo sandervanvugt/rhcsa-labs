f09d53366c30a9a5015f1d2d5938efbaba461642
sandervanvugt <mail@sandervanvugt.nl>
2024-02-29 10:52:26 +0100

    message

--- exam-task14.sh
+++ exam-task14.sh
@@ -1,13 +1,11 @@
 
 ####checking the systemd parts
-if [ -f /home/student/.config/systemd/user/container-mydb.service ] &>/dev/null
+if [ -f /home/student/.config/systemd/user/*.service ] &>/dev/null
 then
         echo -e "\033[32m[OK]\033[0m\t\t systemd user unit was found"
         SCORE=$(( SCORE + 10 ))
 else
-        echo -e "\033[31m[FAIL]\033[0m\t\t systemd user unit was not found. Check the following:"
-        echo -e "-\tuse \033[1mmkdir ~.config/systemd/user\033[0m to create the directory where it must be created"
-        echo -e "-\tuse \033[1mpodman generate systemd\033[0m with the appropriate options to generate it"
+        echo -e "\033[31m[FAIL]\033[0m\t\t systemd user unit was not found."
 fi
 TOTAL=$(( TOTAL + 10 ))
 
@@ -17,7 +15,7 @@ then
         echo -e "\033[32m[OK]\033[0m\t\t the container is currently running as a systemd unit"
         SCORE=$(( SCORE + 10 ))
 else
-        echo -e "\033[31m[FAIL]\033[0m\t\t the container is not currently running as a systemd unit. Did you use \033[1msystemctl enable --user container-mydb.service\033[0m to enable it?"
+        echo -e "\033[31m[FAIL]\033[0m\t\t the container is not currently running as a systemd unit"
 fi
 TOTAL=$(( TOTAL + 10 ))
 
