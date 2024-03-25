e9dbfc8c2af76b85acd55a9fd80c8ec347d43e7f
sandervanvugt <mail@sandervanvugt.nl>
2024-03-25 15:45:34 -0700

    message

--- lab83-grade.sh
+++ lab83-grade.sh
@@ -9,7 +9,7 @@ ls /root &>/dev/null || (echo run this script with root privileges && exit 2)
 #### TASK 1
 echo -e "\033[1mchecking lab 8.3 results\033[0m"
 VGNAME=$(lvs | awk '/root/ { print $2 }')
-if [[ $(lvdisplay /dev/cs/root | awk '/Segment/ { print $2 }') == 2 ]]
+if [[ $(lvdisplay /dev/$VGNAME/root | awk '/Segment/ { print $2 }') == 2 ]]
 then
 	echo -e "\033[32m[OK]\033[0m\t\t found 2 physical volumes in the root LV"
 else
